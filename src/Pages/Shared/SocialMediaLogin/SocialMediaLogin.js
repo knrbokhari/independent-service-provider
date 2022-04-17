cc4ba7ae987fbb3880c8f4e4d5ba43f282eb151d
naeem <kazinaeemrayhan@gmail.com>
2022-04-18 00:56:13 +0600

    react-firebase-hooks, google, facebook, github authentication added

--- src/Pages/Shared/SocialMediaLogin/SocialMediaLogin.js
+++ src/Pages/Shared/SocialMediaLogin/SocialMediaLogin.js
@@ -1,15 +1,48 @@
 import React from "react";
+import {
+  useSignInWithFacebook,
+  useSignInWithGithub,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const SocialMediaLogin = () => {
+  const [signInWithGoogle, user, loading, error1] = useSignInWithGoogle(auth);
+  const [signInWithGithub, error2] = useSignInWithGithub(auth);
+  const [signInWithFacebook, error3] = useSignInWithFacebook(auth);
+
+  let handelError;
+  if (error1 || error2 || error3) {
+    handelError = (
+      <>
+        <p className="text-danger">
+          {error1?.message}
+          {error2?.message}
+          {error3?.message}
+        </p>
+      </>
+    );
+  }
   return (
     <div>
-      <button className="d-block w-100 fs-4 rounded-3 btns buttons">
+      {handelError}
+      <p className="text-danger">{error2?.message}</p>
+      <button
+        onClick={() => signInWithGoogle()}
+        className="d-block w-100 fs-4 rounded-3 btns buttons"
+      >
         Login With Google
       </button>
-      <button className="d-block w-100 fs-4 rounded-3 btns buttons">
+      <button
+        onClick={() => signInWithFacebook()}
+        className="d-block w-100 fs-4 rounded-3 btns buttons"
+      >
         Login With Facebook
       </button>
-      <button className="d-block w-100 fs-4 rounded-3 btns buttons">
+      <button
+        onClick={() => signInWithGithub()}
+        className="d-block w-100 fs-4 rounded-3 btns buttons"
+      >
         Login With GitHub
       </button>
     </div>
