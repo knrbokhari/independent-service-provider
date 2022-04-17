c026bff19fb58ac85a10e9705c17042aaf089a63
naeem <kazinaeemrayhan@gmail.com>
2022-04-18 03:51:25 +0600

    Login, sign up authentication & EmailVerification added

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Form } from "react-bootstrap";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../../firebase.init";
 import SocialMediaLogin from "../../Shared/SocialMediaLogin/SocialMediaLogin";
 import "./Login.css";
 
 const Login = () => {
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+
+  const email = emailRef.current.value;
+  const password = passwordRef.current.value;
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(email, password);
+    console.log(user);
+  };
   return (
     <div className="container">
       <h2
@@ -13,16 +29,28 @@ const Login = () => {
       >
         Login
       </h2>
+
+      <h2 className="text-danger text-center">{user?.user?.email}</h2>
+      <h2 className="text-danger text-center">{error?.message}</h2>
+
       <div className="form">
-        <Form>
+        <Form onSubmit={handleLogin}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              ref={emailRef}
+              type="email"
+              placeholder="Enter email"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              ref={passwordRef}
+              type="password"
+              placeholder="Password"
+            />
           </Form.Group>
           <p className="my-2 fs-4 text-white">
             Don't have an account?{" "}
--- src/Pages/Login/Registration/Registration.js
+++ src/Pages/Login/Registration/Registration.js
@@ -1,9 +1,44 @@
-import React from "react";
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from "firebase/auth";
+import React, { useRef, useState } from "react";
 import { Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import auth from "../../../firebase.init";
 import SocialMediaLogin from "../../Shared/SocialMediaLogin/SocialMediaLogin";
 
 const Registration = () => {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [error, setError] = useState("");
+
+  const verifiEmail = async () => {
+    await sendEmailVerification(auth.currentUser);
+    alert("Verification email sent");
+  };
+
+  const handleRegistration = async (e) => {
+    e.preventDefault();
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    console.log(password.length);
+    if (password.length < 6) {
+      setError("give 6 or more words for password");
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        verifiEmail();
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error?.message);
+      });
+  };
   return (
     <div>
       <h2
@@ -13,25 +48,31 @@ const Registration = () => {
         Registration
       </h2>
       <div className="form">
-        <Form>
+        <Form onSubmit={handleRegistration}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              ref={emailRef}
+              type="email"
+              placeholder="Enter email"
+            />
           </Form.Group>
-
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              ref={passwordRef}
+              type="password"
+              placeholder="Password"
+            />
           </Form.Group>
+          <p className="text-danger">{error}</p>
           <p className="my-2 fs-4 text-white">
             Already have an account?{" "}
             <Link to="/login" className="text-decoration-none">
               Please Login
-            </Link>{" "}
+            </Link>
           </p>
-          <button type="submit" className="btns buttons mt-2  w-100">
-            Registration
-          </button>
+          <button className="btns buttons mt-2  w-100">Registration</button>
         </Form>
         <div className="d-flex align-items-center mt-3">
           <div className="line"></div>
